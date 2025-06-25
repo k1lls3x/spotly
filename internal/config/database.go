c470ce5dcb726f7fae0eb3d0fe680868f8b5c528
Дмитрий <118112928+k1lls3x@users.noreply.github.com>
2025-06-25 20:52:46 +0300

    Refactor project structure and docs

---
+++ internal/config/database.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/rs/zerolog/log"
+	"os"
+)
+
+func Init() *sqlx.DB {
+	_ = godotenv.Load()
+
+	cfg := LoadConfig()
+	db, err := sqlx.Connect("postgres", cfg.DSN())
+	if err != nil {
+		log.Error().Err(err).Msg("❌ Ошибка подключения к PostgreSQL")
+		os.Exit(1)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Error().Err(err).Msg("❌ PostgreSQL не отвечает")
+		os.Exit(1)
+	}
+
+	log.Info().Msg("✅ Подключение к PostgreSQL успешно")
+	return db
+}
